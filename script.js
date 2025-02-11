9f48599f2268e8dfc6fcaa0122047fa45fc0b9d1
Amit <81560269+immeas@users.noreply.github.com>
2025-02-11 12:54:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,37 +1,43 @@
 //complete this code
 class Person {
 	constructor(name, age){
-		this.name = name;
-		this.age = age;
+		this._name = name;
+		this._age = age;
 	}
 
 	get name(){
-		return this.name;
+		return this._name;
 	}
 
 	set age(){
-		if (typeof newAge === "number" && newAge > 0) {
-      this._age = newAge;
-    } else {
-      console.log("Please enter a valid age.");
-    }
-	}
-	}
+		return this._age
 
 	}
 
 class Student extends Person {
 	study(){
-		console.log(`${this.name} is studying`);
+		console.log(`${this._name} is studying`);
 	}
 
 }
 
 class Teacher extends Person {
 	teach(){
-		console.log(`${this.name} is teaching`)
+		console.log(`${this._name} is teaching`)
 	}
 }
+
+const person = new Person("John", 25);
+console.log(person.name); 
+
+person.age = 30; 
+console.log(person.age); 
+
+const student = new Student("Alice", 22);
+student.study(); 
+
+const teacher = new Teacher("Bob", 40);
+teacher.teach();
 
 // Do not change the code below this line
 window.Person = Person;
